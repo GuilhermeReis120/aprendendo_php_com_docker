4e8781be812b9c79653891fb7b764d47fdd71ffa
GuilhermeReis120 <souzaguireis@gmail.com>
2026-06-09 20:28:42 -0300

    (fix) Correção na estrutura do projeto

---
+++ www/1_intro/toDo_cli/toDo_cli.php
@@ -0,0 +1,7 @@
+<?php
+echo "Hello, World!";
+echo "==============================\n";
+echo "Bem-vindo ao ToDo CLI!\n";
+echo "==============================\n";
+echo "Digite 'help' para ver os comandos disponíveis.\n";
+$tasks = [];
